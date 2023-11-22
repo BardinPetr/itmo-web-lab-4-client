907f35a05dc19285dfd055d6b6728d8d814ba301
BardinPetr <bardin.petr@gmail.com>
2023-11-22 05:02:00 +0300

    ecosystem fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  idcUrl: "http://idp.weblab4.bardinpetr.ru/realms/lab4",
+  idcUrl: "https://idp.bardinpetr.ru/realms/lab4",
   idcClientId: "web-lab4-server",
   idcClientSecret: "W6yIrCWcD0WIZTlZ0tyeCprIgIjikvfb",
-  apiUrl: "http://weblab4.bardinpetr.ru"
+  apiUrl: "https://weblab4.bardinpetr.ru"
 }
