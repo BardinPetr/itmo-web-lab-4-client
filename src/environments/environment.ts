f7dd3d959abe7cdc81bceec02388995d2424eee8
BardinPetr <bardin.petr@gmail.com>
2023-12-16 16:41:41 +0300

    fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   idcUrl: "http://keycloak:8090/realms/lab4",
   idcClientId: "web-lab4-server",
   idcClientSecret: "W6yIrCWcD0WIZTlZ0tyeCprIgIjikvfb",
-  apiUrl: "http://weblab4.bardinpetr.ru"
+  apiUrl: "http://localhost:8080"
 }
